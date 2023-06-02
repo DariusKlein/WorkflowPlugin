070d959be0d2b596929d1a77ed6760422b5831b1
Darius <darius.klein@student.hu.nl>
2023-06-02 20:43:48 +0200

    rename

--- src/com.darius.workflow.sdPlugin/app.js
+++ src/com.darius.workflow.sdPlugin/app.js
@@ -1,7 +1,7 @@
 /// <reference path="libs/js/action.js" />
 /// <reference path="libs/js/stream-deck.js" />
 
-const myAction = new Action('com.darius.workflow.test.action');
+const test = new Action('com.darius.workflow.test.action');
 
 /**
  * The first event fired when Stream Deck starts
@@ -10,10 +10,10 @@ $SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) =
 	console.log('Stream Deck connected!');
 });
 
-myAction.onKeyUp(({ action, context, device, event, payload }) => {
+test.onKeyUp(({ action, context, device, event, payload }) => {
 	console.log('Your key code goes here!');
 });
 
-myAction.onDialRotate(({ action, context, device, event, payload }) => {
+test.onDialRotate(({ action, context, device, event, payload }) => {
 	console.log('Your dial code goes here!');
 });
